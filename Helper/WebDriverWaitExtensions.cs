6ce032d8bbc56a17290ba56449af1871b50194ee
monleak <hieu16112001@gmail.com>
2024-11-15 01:51:10 +0700

    update

---
+++ Helper/WebDriverWaitExtensions.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Core.Helper;
+
+public static class WebDriverWaitExtensions
+{
+    public static IWebElement AnyOf(this WebDriverWait wait, params Func<IWebDriver, IWebElement>[] conditions)
+    {
+        return wait.Until(driver =>
+        {
+            foreach (var condition in conditions)
+            {
+                try
+                {
+                    IWebElement element = condition(driver);
+                    if (element != null)
+                    {
+                        return element;
+                    }
+                }
+                catch{}
+            }
+            return null; // Không tìm thấy điều kiện nào thỏa mãn
+        });
+    }
+}
--- Models/ManageChrome.cs
+++ Models/ManageChrome.cs
@@ -32,7 +32,7 @@ public partial class ManageChrome : ObservableRecipient
 
     public readonly string pathProfile;
 
-    private IWebDriver _driver;
+    protected IWebDriver _driver;
 
     [ObservableProperty]
     private string displayname;
@@ -65,31 +65,39 @@ public partial class ManageChrome : ObservableRecipient
         StatusDescription = StatusDescriptions.GetValueOrDefault(status, "Không xác định");
     }
 
-    public void InitChrome(Action<Action> executeInMainThread = null)
+    public void InitChrome(Action<Action> executeInMainThread = null, string proxy = null)
     {
-        ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-        service.EnableVerboseLogging = false;
-        service.HideCommandPromptWindow = true;
+        if(_driver == null)
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.EnableVerboseLogging = false;
+            service.HideCommandPromptWindow = true;
 
-        ChromeOptions options = new ChromeOptions();
-        options.AddArgument($"--user-data-dir={pathProfile}");
-        options.AddArgument("--disable-gpu");
-        options.AddArgument("--disable-dev-shm-usage");
-        options.AddArgument("--disable-infobars");
-        options.AddArgument("--disable-plugins");
-        options.AddArgument("--disable-notifications");
-        options.AddArgument("--disable-background-networking");
-        options.AddArgument("--disable-default-apps");
-        options.AddArgument("--disable-sync");
-        options.AddArgument("--window-size=945,1012");
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument($"--user-data-dir={pathProfile}");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-infobars");
+            options.AddArgument("--disable-plugins");
+            options.AddArgument("--disable-notifications");
+            options.AddArgument("--disable-background-networking");
+            options.AddArgument("--disable-default-apps");
+            options.AddArgument("--disable-sync");
+            options.AddArgument("--window-size=945,1012");
 
-        _driver = new ChromeDriver(service,options);
-        _PID = service.ProcessId;
+            if(!string.IsNullOrEmpty(proxy))
+            {
+                options.AddArgument($"--proxy-server={proxy}");
+            }
 
-        executeInMainThread?.Invoke(() => updateStatus(ManageChromeStatus.OpenChrome, true));
+            _driver = new ChromeDriver(service, options);
+            _PID = service.ProcessId;
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            executeInMainThread?.Invoke(() => updateStatus(ManageChromeStatus.OpenChrome, true));
+        }
     }
 
-    public void ExecuteAction(Func<IWebDriver, object> action, out object result, Action<Action> executeInMainThread = null)
+    public void ExecuteAction(Func<IWebDriver, object, object> action, out object result, object paramForAction = null, Action<Action> executeInMainThread = null)
     {
         result = null;
         if (_driver is IWebDriver driver)
@@ -97,7 +105,7 @@ public partial class ManageChrome : ObservableRecipient
             try 
             {
                 executeInMainThread?.Invoke(() => updateStatus(ManageChromeStatus.Running, true));
-                result = action(driver);
+                result = action(driver,paramForAction);
                 executeInMainThread?.Invoke(() => updateStatus(ManageChromeStatus.Stop, true));
             }
             catch (NoSuchWindowException)
@@ -109,7 +117,7 @@ public partial class ManageChrome : ObservableRecipient
                 }
                 else
                 {
-                    QuitBrowser();
+                    QuitBrowser(executeInMainThread);
                 }
             }
             catch(Exception)
@@ -123,9 +131,9 @@ public partial class ManageChrome : ObservableRecipient
         }
     }
 
-    public void QuitBrowser()
+    public void QuitBrowser(Action<Action> executeInMainThread = null, bool force = false)
     {
-        if(status == ManageChromeStatus.Running)
+        if(status == ManageChromeStatus.Running && !force)
         {
             return;
         }
@@ -145,7 +153,14 @@ public partial class ManageChrome : ObservableRecipient
             process.Kill();
         }
         catch { }
-        updateStatus(ManageChromeStatus.CloseChrome,true);
+        if(executeInMainThread == null)
+        {
+            updateStatus(ManageChromeStatus.CloseChrome);
+        }
+        else
+        {
+            executeInMainThread?.Invoke(() => updateStatus(ManageChromeStatus.CloseChrome, true));
+        }
     }
 
     private bool checkDriver()
@@ -170,6 +185,12 @@ public partial class ManageChrome : ObservableRecipient
             updateStatus(ManageChromeStatus.CloseChrome,true);
         }
         updateStatusDescription();
+    }
+
+    public bool CanStartWork()
+    {
+        Refresh();
+        return status == ManageChromeStatus.OpenChrome || status == ManageChromeStatus.Stop;
     }
 
     public bool CanRemoveProfile()
